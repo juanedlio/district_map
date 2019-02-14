1b652abbd3f2c4c171eb5359468d6a46911f7e57
Jordan Lamoreaux <jordan@Jordans-MacBook-Air.local>
2019-02-14 10:24:27 -0600

    Initial commit

---
+++ js/schools.js
@@ -0,0 +1,21 @@
+var highSchools = [
+  { 'name' : 'South Austin HS', 'phone' : '(512)555-2002', 'id' : 0},
+  { 'name' : 'North Austin HS', 'phone' : '(512)555-2001', 'id' : 1}
+];
+
+var middleSchools = [
+  { 'name' : 'NorthWest Austin MS', 'phone' : '(512)555-1001', 'id' : 0},
+  { 'name' : 'NorthEast Austin MS', 'phone' : '(512)555-1002', 'id' : 1},
+  { 'name' : 'SouthEast Austin MS', 'phone' : '(512)555-1004', 'id' : 2},
+  { 'name' : 'SouthWest Austin MS', 'phone' : '(512)555-1003', 'id' : 3}
+];
+
+var elementarySchools = [
+  { 'name' : 'South Austin ES', 'phone' : '(512)555-0001', 'id' : 0},
+  { 'name' : 'West Austin ES', 'phone' : '(512)555-0003', 'id' : 1},
+  { 'name' : 'North Austin ES', 'phone' : '(512)555-0002', 'id' : 2},
+  { 'name' : 'East Austin ES', 'phone' : '(512)555-0004', 'id' : 3},
+  { 'name' : 'Central Austin ES', 'phone' : '(512)555-0005', 'id' : 4}
+];
+
+var schools = [highSchools, middleSchools, elementarySchools];
