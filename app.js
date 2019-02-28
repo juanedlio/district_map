2ad0990da66e1dac5d1ae9c214a156c25d638b98
Jordan Lamoreaux <jnacious88@gmail.com>
2019-02-27 21:15:33 -0600

    remove hostname specification

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 const path = require('path');
 const router = express.Router();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+const port = process.env.PORT;
 
 app.use(express.static(__dirname));
 
@@ -13,6 +13,4 @@ app.get('/', function (req, res) {
   res.sendFile('/index.html');
 })
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(process.env.PORT || 5000, console.log(`Server running on port ${port}`));
