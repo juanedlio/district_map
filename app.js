ae4c899387df80fbafacffb894fb69562bbaf42b
Jordan Lamoreaux <jnacious88@gmail.com>
2019-02-27 20:59:44 -0600

    correct start script

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const router = express.Router();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
