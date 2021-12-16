95d08944fa3a4c179798d9c355de0b4dfb9707e5
Karina Carvajal <karina@edlio.com>
2021-12-16 11:40:36 -0800

    add new school IDs

--- js/schools.js
+++ js/schools.js
@@ -1,8 +1,8 @@
 const schools = [
-  { 'name' : 'Beverly Hills High School', 'phone' : '(310) 551-5100 Ext. 8000', 'address' : '241 Moreno Drive Beverly Hills, CA 90212', 'id' : 5},
-  { 'name' : 'Beverly Vista School', 'phone' : '(310) 229-3665', 'address' : '200 South Elm Drive Beverly Hills, CA 90212', 'id' : 1},
-  { 'name' : 'El Rodeo School', 'phone' : '(310) 229-3670', 'address' : '605 Whittier Drive Beverly Hills, CA 90210', 'id' : 2},
-  { 'name' : 'Horace Mann Elementary School', 'phone' : '(310) 551-5104', 'address' : '8701 Charleville Blvd. Beverly Hills, CA 90211', 'id' : 4},
-  { 'name' : 'Hawthorne Elementary School', 'phone' : '(512)555-0003', 'address' : '624 North Rexford Drive Beverly Hills, CA 90210', 'id' : 3},
-  { 'name' : 'Moreno Continuation High School', 'phone' : '(310) 551-5100 x2324', 'address' : '255 South Lasky Drive Beverly Hills, CA 90212', 'id' : 6}
+  { 'name' : 'Beverly Hills High School', 'phone' : '(310) 551-5100 Ext. 8000', 'address' : '241 Moreno Drive Beverly Hills, CA 90212', 'id' : 301},
+  { 'name' : 'Beverly Vista School', 'phone' : '(310) 229-3665', 'address' : '200 South Elm Drive Beverly Hills, CA 90212', 'id' : 304},
+  { 'name' : 'El Rodeo School', 'phone' : '(310) 229-3670', 'address' : '605 Whittier Drive Beverly Hills, CA 90210', 'id' : 305},
+  { 'name' : 'Horace Mann Elementary School', 'phone' : '(310) 551-5104', 'address' : '8701 Charleville Blvd. Beverly Hills, CA 90211', 'id' : 302},
+  { 'name' : 'Hawthorne Elementary School', 'phone' : '(512)555-0003', 'address' : '624 North Rexford Drive Beverly Hills, CA 90210', 'id' : 303},
+  { 'name' : 'Moreno Continuation High School', 'phone' : '(310) 551-5100 x2324', 'address' : '255 South Lasky Drive Beverly Hills, CA 90212', 'id' : 8380}
 ];
